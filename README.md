963370199be71fc8527622a47961fab8f7ba5d3c
Jocimar Lopes <lopes.jocimar@gmail.com>
2023-01-13 10:32:17 -0300

    Init project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Texo IT Project
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  MAIN_URL: 'https://tools.texoit.com/backend-java/api/movies'
 };
 
 /*
