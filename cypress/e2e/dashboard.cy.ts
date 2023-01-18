1c35ddf211c2b52f673bb08e600fc91eb848d748
Jocimar Lopes <lopes.jocimar@gmail.com>
2023-01-18 19:00:26 -0300

    refac/testes_unitarios_e_documentacao

--- cypress/e2e/dashboard.cy.ts
+++ cypress/e2e/dashboard.cy.ts
@@ -1,15 +1,21 @@
 describe('Frontend Angular Test', () => {
-  it('Indo para a página Dashboard', () => {
-    cy.visit('http://localhost:4200/')
-    cy.get(':nth-child(1) > .item').click()
-    cy.log('Procurando por ganhadores do "Ano 2000"... Aguarde')
+  const url = 'http://localhost:4200/'
+  it('Pesquisando ganhadores do "ano 2000"', () => {
+    cy.visit(url)
+    cy.get(':nth-child(1) > .item').click({force: true})
     cy.get('.searchbar-input').type('2000')
-    cy.get('.button-search').click()
+    cy.get('.button-search').click({force: true})
     cy.log('Observe a resposta do servidor na lista...')
     cy.wait(2000)
-    cy.get('.searchbar-clear-button').click()
+  })
+
+  it('Procurando ganhadores do "Ano 2005"', () => {
+    cy.visit(url)
+    cy.get(':nth-child(1) > .item').click({force: true})
+    cy.get('.searchbar-clear-button').click({force: true})
     cy.get('.searchbar-input').type('2005')
-    cy.get('.button-search').click()
-    cy.log('Observe a resposta do servidor na lista, again...')
+    cy.get('.button-search').click({force: true})
+    cy.log('Observe a resposta do servidor na lista...')
+    cy.wait(2000)
   })
 })
--- cypress/e2e/list.cy.ts
+++ cypress/e2e/list.cy.ts
@@ -1,47 +1,59 @@
 describe('Frontend Angular Test', () => {
-  it('Go to List Page', () => {
-    cy.visit('http://localhost:4200/')
-
-    cy.log('Clicando no item "List" do menu lateral')
-    cy.wait(1000)
-    cy.get(':nth-child(2) > .item').click()
-
+  const url = 'http://localhost:4200/list'
+  
+  it('Clicando no botão de avançar página', () => {
+    cy.visit(url)
     cy.log('Clicando no botão de avançar página')
     cy.wait(1500)
     cy.get('#after').click({force: true})
     cy.wait(500)
     cy.get('#after').click({force: true})
     cy.wait(500)
+  })
 
-    cy.log('Clicando no botão que vai para a última página')
+  it('Clicando no botão que vai para a última página', () => {
+    cy.visit(url)
     cy.wait(1000)
     cy.get('#last').click({force: true})
+  })
 
-    cy.log('Clicando no botão que vai para a primeira página')
+  it('Clicando no botão que vai para a primeira página', () => {
+    cy.visit(url)
     cy.wait(1000)
     cy.get('#first').click({force: true})
-    
-    cy.log('Acessando a página 7 da paginação')
-    cy.wait(1000)
-    cy.get('pagination > div > :nth-child(9)').click()
+  })
 
-    cy.log('Pesquisar ano 2003 no Filtro')
+  it('Acessando página aleatória da paginação', () => {
+    cy.visit(url)
+    cy.wait(1000)
+    cy.get('pagination > div > :nth-child(9)').click({force: true})
+  })
+
+  it('Pesquisar ano 2003 no Filtro', () => {
+    cy.visit(url)
     cy.wait(1000)
     cy.get('.searchbar-input').type('2003')
-
-    cy.log('Apagando o filtro de ano e filtrando por Ganhador')
-    cy.wait(1500)
+    cy.log('Apagando ano de pesquisa')
+    cy.wait(4000)
     cy.get('.searchbar-clear-button').click()
-    cy.get('#filter-winner').click({force: true})
-    cy.wait(200)
-    cy.get('#alert-input-1-0 > .alert-button-inner').click({force: true})
-    cy.get('.alert-button-group > :nth-child(2)').click({force: true})
+  })
 
-    cy.log('Filtrando por não ganhador')
+  it('Filtrando por Ganhador', () => {
+    cy.visit(url)
     cy.wait(1000)
     cy.get('#filter-winner').click({force: true})
-    cy.get('#alert-input-2-1 > .alert-button-inner').click()
+    cy.wait(500)
+    cy.get('#alert-input-1-0 > .alert-button-inner').click({force: true})
     cy.get('.alert-button-group > :nth-child(2)').click({force: true})
+    cy.wait(1000)
+  })
 
+  it('Filtrando por Não-Ganhador', () => {
+    cy.visit(url)
+    cy.wait(1000)
+    cy.get('#filter-winner').click({force: true})
+    cy.get('#alert-input-1-1 > .alert-button-inner').click({force: true})
+    cy.get('.alert-button-group > :nth-child(2)').click({force: true})
+    cy.wait(1000)
   })
 })
